01e3d8b48d96bffcb022eaa4653b43448b2a67a4
JinDamanee <88868930+JinDamanee2544@users.noreply.github.com>
2023-01-03 00:42:10 +0700

    Feat: Base Server

--- server.go
+++ server.go
@@ -3,9 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
+
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello, World!")
+	})
+
+	// Start server
+	e.Logger.Fatal(e.Start(":2565"))
 }
